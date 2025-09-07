f9d39dbc8b8bfa36b963f9edfbc8b6605d1d60bf
Laih0 <stvrzf2k16@gmail.com>
2025-09-07 19:08:17 +0300

    fix: vectore icon

--- src/routes/screens/HomeScreen.jsx
+++ src/routes/screens/HomeScreen.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { Ionicons } from '@react-native-vector-icons/ionicons';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Bienvenue sur E-tsabo 🌿</Text>
       <Text style={styles.subtitle}>Votre santé, entre vos mains</Text>
-      <Ionicons name="home" size={100} color="#2E7D32" />
     </View>
   );
 }
